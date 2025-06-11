f68128093accad954e9883d27e29bc6413af053e
sanchos999 <sanchos999@gmail.com>
2025-06-11 15:03:07 +0300

    Update MainActivity.java

--- app/src/main/java/com/hdmi/launcher/MainActivity.java
+++ app/src/main/java/com/hdmi/launcher/MainActivity.java
@@ -3,14 +3,23 @@ package com.hdmi.launcher;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("HDMITEST", "MainActivity: запускаю HdmiSwitcherService и финиширую");
-        startService(new Intent(this, HdmiSwitcherService.class));
-        finish(); // ОБЯЗАТЕЛЬНО!
+
+        // Показываем пустой layout (важно для HOME activity)
+        setContentView(new LinearLayout(this));
+
+        // Стартуем сервис через небольшую задержку (или сразу)
+        new Handler().postDelayed(() -> {
+            Log.d("HDMITEST", "MainActivity: запускаю HdmiSwitcherService");
+            startService(new Intent(this, HdmiSwitcherService.class));
+            // НЕ вызываем finish()!
+        }, 500);
     }
 }
