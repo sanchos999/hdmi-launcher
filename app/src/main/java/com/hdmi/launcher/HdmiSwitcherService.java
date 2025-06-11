d696f16a841fab0ecc70b04930d17d2d41e38040
sanchos999 <sanchos999@gmail.com>
2025-06-11 15:04:32 +0300

    Update HdmiSwitcherService.java

--- app/src/main/java/com/hdmi/launcher/HdmiSwitcherService.java
+++ app/src/main/java/com/hdmi/launcher/HdmiSwitcherService.java
@@ -2,35 +2,31 @@ package com.hdmi.launcher;
 
 import android.app.Service;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 
 public class HdmiSwitcherService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("HDMITEST", "Service стартовал, жду 10 сек...");
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Log.d("HDMITEST", "Прошло 10 сек, пытаюсь переключить HDMI");
-                try {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse("content://android.media.tv/passthrough/com.oneway.tv/.hdmi.HDMIInputService/HW5"));
-                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(i);
-
-                    Log.d("HDMITEST", "Интент HDMI отправлен из сервиса");
-                } catch (Exception e) {
-                    Log.e("HDMITEST", "Ошибка: " + e.toString());
-                }
-                stopSelf();
+        Log.d("HDMITEST", "HdmiSwitcherService: эмулируем KEYCODE_TV_INPUT (243)");
+        new Thread(() -> {
+            try {
+                // Эмуляция нажатия "KEYCODE_TV_INPUT"
+                Process proc = Runtime.getRuntime().exec(new String[]{"input", "keyevent", "243"});
+                int result = proc.waitFor();
+                Log.d("HDMITEST", "Keyevent отправлен, код выхода: " + result);
+            } catch (Exception e) {
+                Log.e("HDMITEST", "Ошибка при эмуляции keyevent: " + Log.getStackTraceString(e));
             }
-        }, 10000);
+            stopSelf();
+        }).start();
+
+        // Не пересоздавать сервис
         return START_NOT_STICKY;
     }
 
     @Override
-    public IBinder onBind(Intent intent) { return null; }
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
